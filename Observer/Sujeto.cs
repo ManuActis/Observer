106ce85940bc04adcf641987719a83a277154c14
Manuel Actis <78481353+ManuActis@users.noreply.github.com>
2022-05-19 12:56:12 -0300

    Agregar archivos de proyecto.

---
+++ Observer/Sujeto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    class Sujeto
+    {
+        private List<IObservador> observadores = new List<IObservador>();
+        private string mensaje;
+        private Random rnd = new Random();
+        private int num;
+
+        public int Numero { get => num; set => num = value; }
+
+        public void Suscribir(IObservador suscrito)
+        {
+            observadores.Add(suscrito);
+        }
+
+        public void Desuscribir(IObservador suscrito)
+        {
+            observadores.Remove(suscrito);
+        }
+
+        public void Notificar()
+        {
+            foreach (IObservador obs in observadores) {
+                obs.Actualizar(mensaje);
+            } 
+        }
+
+        public void Accion()
+        {
+            // acá va la acción propia del negocio
+
+            num = rnd.Next(10);
+
+            if(num%2==0)
+            {
+                Console.WriteLine("Nuevo valor");
+                mensaje = String.Format("El nuevo valor es {0}", num);
+                Notificar();
+            }
+        }
+}
